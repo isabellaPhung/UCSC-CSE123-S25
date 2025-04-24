d20c98b66a868b2d43c0f7984e88727b9bc5aca3
Lennan Tuffy <ltuffy@ucsc.edu>
2025-04-24 11:32:51 -0700

    added a test directory for mqtt_conn

---
+++ esp-tentative-name/components/mqtt_conn/test_apps/main/app_main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+#include "esp_log.h"
+
+void aws_iot_demo_main(void *pvParameters);
+void lcd_task(void *pvParameters);
+
+struct {
+  SemaphoreHandle_t buffer_sem;
+  char payload[512];
+  size_t payload_len;
+} task_params;
+
+static const char *TAG = "MQTT_EXAMPLE";
+
+/*
+ * Prototypes for the demos that can be started from this project.  Note the
+ * MQTT demo is not actually started until the network is already.
+ */
+
+void app_main()
+{
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %"PRIu32" bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+
+    /* Initialize NVS partition */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        /* NVS partition was truncated
+         * and needs to be erased */
+        ESP_ERROR_CHECK(nvs_flash_erase());
+
+        /* Retry nvs_flash_init */
+        ESP_ERROR_CHECK(nvs_flash_init());
+    }
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    // TODO replace with a more complete wifi connection function that supports low power mode
+    ESP_ERROR_CHECK(example_connect());
+
+    vSemaphoreCreateBinary(task_params.buffer_sem);
+
+    xTaskCreate(aws_iot_demo_main, "AWS_DEMO", 4069, (void *)&task_params, 5, NULL);
+}
