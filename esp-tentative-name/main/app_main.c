14104cfe9db3f4f41bb96cd988ea61885fad626c
Lennan Tuffy <ltuffy@ucsc.edu>
2025-04-30 21:17:18 -0700

    added mqtt function to main and simplified, added guide for connecting components

--- esp-tentative-name/main/app_main.c
+++ esp-tentative-name/main/app_main.c
@@ -3,6 +3,8 @@
 #include <stddef.h>
 #include <string.h>
 
+#include "mqtt_conn.h"
+
 #include "esp_system.h"
 #include "nvs_flash.h"
 #include "esp_event.h"
@@ -13,56 +15,175 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 
+#include "cJSON.h"
+
 #include "esp_log.h"
 
-void aws_iot_demo_main(void *pvParameters);
-void DB_task_demo(void *pvParameters);
+static const char *TAG = "MAIN";
 
-struct
-{
-    SemaphoreHandle_t buffer_sem;
-    char payload[512];
-    size_t payload_len;
-} task_params;
+struct callback_data_t {
+  int expected;
+  int cur_index;
+  // TODO
+  // struct database_t *db_ptr;
+};
 
-static const char *TAG = "DEMO";
+void demo_callback(const char *payload, size_t payload_length, void *cb_data) {
+  struct callback_data_t *data = (struct callback_data_t *) cb_data;
+  cJSON *root = cJSON_ParseWithLength(payload, payload_length);
+  cJSON *item;
+  item = cJSON_GetObjectItem(root, "id");
+  if (item && (strcmp(item->valuestring, "server") == 0)){
+    item = cJSON_GetObjectItem(root, "action"); 
+    if (item && (strcmp(item->valuestring, "length") == 0)) {
+      item = cJSON_GetObjectItem(root, "length");
+      if (item && cJSON_IsNumber(item)) {
+        data->expected = item->valueint;
+        data->cur_index = 0;
+        ESP_LOGI(TAG, "Expecting %d tasks", data->expected);
+      }
+    }
+    else if (item && (strcmp(item->valuestring, "response") == 0)){
+      ESP_LOGI(TAG, "Server response index %d", data->cur_index);
+      // TODO 
+      // parsetaskjson(data->db_ptr, payload, payload_size);
+      data->cur_index ++;
+    }
+  }
+  cJSON_Delete(root);
+  ESP_LOGI(TAG, "Callback function called\n");
+}
 
-/*
- * Prototypes for the demos that can be started from this project.  Note the
- * MQTT demo is not actually started until the network is already.
- */
+#define BACKUP_PAYLOAD "{\"id\":\"c72572d0-8c8c-4f37-8ff6-829cac2eabec\",\"action\":\"refresh\"}"
+#define BACKUP_PAYLOAD_LENGTH ((size_t) (sizeof(BACKUP_PAYLOAD) - 1))
+#define RETRY_DELAY_MS 5000U
 
-void app_main()
-{
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+int request_backup(struct callback_data_t *cb_data){
+  int return_status = EXIT_SUCCESS;
+  size_t retries = 4;
+  mqtt_connect();
+  mqtt_subscribe();
 
-    esp_log_level_set("*", ESP_LOG_INFO);
+  mqtt_publish(BACKUP_PAYLOAD, BACKUP_PAYLOAD_LENGTH);
+  while (1) {
+    mqtt_loop(5000);
+    retries--;
+    if (retries == 0){
+      return_status = EXIT_FAILURE;
+      ESP_LOGW(TAG, "Three failures of backup request, trying again later");
+      break;
+    }
+    if (cb_data->expected == 0) {
+      ESP_LOGI(TAG, "Did not get back a length message from the server, re-publishing after a delay");
+      vTaskDelay(RETRY_DELAY_MS / portTICK_PERIOD_MS);
+      mqtt_publish(BACKUP_PAYLOAD, BACKUP_PAYLOAD_LENGTH);
+    }
+    else if (cb_data->expected != cb_data->cur_index) {
+      ESP_LOGI(TAG, "Did not get the expected amount, listening for some more time");
+    }
+    else {
+      break;
+    }
+  }
 
-    /* Initialize NVS partition */
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        /* NVS partition was truncated
-         * and needs to be erased */
-        ESP_ERROR_CHECK(nvs_flash_erase());
+  mqtt_unsubscribe();
+  mqtt_disconnect();
+  return return_status;
+}
 
-        /* Retry nvs_flash_init */
-        ESP_ERROR_CHECK(nvs_flash_init());
+// TODO
+#if 0
+struct task_t {
+  // task data
+  // probably already defined in db header
+};
+
+struct db_t {
+  // db as a struct
+  // also probably defined in db header
+};
+
+#define TASK_LIST_SIZE 7
+
+struct packed_t {
+  struct task_t on_screen[TASK_LIST_SIZE],
+  bool req_db_updates,
+  bool push_new_completions_local,
+  bool push_new_completions_mqtt
+};
+#endif
+
+void app_main() {
+  esp_log_level_set("*", ESP_LOG_INFO);
+
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+  }
+
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  ESP_ERROR_CHECK(example_connect());
+
+  
+  struct callback_data_t cb_data;
+  cb_data.expected = 0;
+  cb_data.cur_index = -1;
+  // TODO
+  // struct db_t database;
+  // cb_data.db_ptr = &database;
+  // struct packed_t ui_data;
+
+  int return_status;
+  return_status = mqtt_init(&demo_callback, (void *) &cb_data);
+  assert (return_status == EXIT_SUCCESS);
+
+  // TODO
+  // initDB(&database);
+  // init_ui(&ui_data);
+  
+  // TODO refill on_screen with the 0th page of tasks
+  // retrievetaskssorted(&database, &(ui_data.on_screen), TASK_LIST_SIZE);
+
+  request_backup(&cb_data);
+
+#if 0
+  long frame_timer = 0;
+  while (1){
+    // update the screen
+    frame_timer++;
+
+    // if there has been inputs where the user needs new tasks 
+    // on the screen, modify the req_db_updates variables inside 
+    // of the ui library
+    if (req_db_updates) {
+      // refill the on_screen array
+      retrievetaskssorted(&db, &on_screen, TASK_LIST_SIZE);
+      req_db_updates = false;
     }
 
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    // if tasks have been marked as completed this frame, modify
+    // the push_new_completions variable inside the ui library
+    if (push_new_completions_local) {
+      // push changes to local db
+      // push changes to disk
+      push_new_completions_local = false;
+      push_new_completions_mqtt = true;
+      // possibly mark how many and/or which ones need to be pushed to the server
+    }
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    // TODO replace with a more complete wifi connection function that supports low power mode
-    // ESP_ERROR_CHECK(example_connect());
-
-    vSemaphoreCreateBinary(task_params.buffer_sem);
-
-    xTaskCreate(DB_task_demo, "DB_Task", 65536, NULL, 5, NULL);
+    if (frame_timer >= 900 seconds) {
+        if (push_new_completions_mqtt){
+          // publish the "these tasks have been updates" payload
+          push_new_completions_mqtt = false;
+        }
+        // publish the "need new tasks" payload
+        request_backup(&cb_data);
+        frame_timer = 0;
+    }
+  }
+#endif
 }
