e3540910db81f80da36a126d2a2c2f512684fcf8
marobeck <marobeck@ucsc.edu>
2025-05-05 02:58:27 -0700

    Added adjustable offset for task sorting

--- esp-tentative-name/components/database/src/defs.h
+++ esp-tentative-name/components/database/src/defs.h
@@ -4,6 +4,12 @@
 #define MAX_NAME_SIZE 128
 #define MAX_DESC_SIZE 1024
 
+/// Priority is an offset parameter, this value decides how many seconds difference each point
+/// of priority is worth.
+/// Set to a quarter of a day, such that a task with a priority of 4 would be seen as having a due 
+/// date of 1 day sooner by the sorting algorithm.
+#define PRIORITY_MULTIPLIER "21600"
+
 #define UUID_LENGTH 37 // 36 characters + null terminator
 
 #define MOUNT_POINT "/sdcard"
