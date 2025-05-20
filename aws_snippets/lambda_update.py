5942dd3a0de098bb8938d11837fc103974f5ec12
Lennan Tuffy <ltuffy@ucsc.edu>
2025-05-19 17:32:59 -0700

    made habits modifiable

--- aws_snippets/lambda_update.py
+++ aws_snippets/lambda_update.py
@@ -6,19 +6,25 @@ s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
     device_id = event["id"]
-    obj = s3.Object("iot-device-data-backups", device_id + "/task0.json")
+    data_type = event["type"]
+    obj = s3.Object("iot-device-data-backups", device_id + "/" + data_type + "0.json")
     data = json.loads(obj.get()["Body"].read().decode("utf-8"))
-    arr = data["task"]
+    arr_s3 = data[data_type]
 
-    for task in event["task"]:
-        # if you don't want to not allow the device to un-complete an event
-        #if task["completion"] == 1:
-        #    continue
-        uuid = task["id"]
-        for task_db in data["task"]:
-            # do not allow modifying a deleted task
-            if task_db["id"] == uuid and task_db["completion"] != 0:
-                task_db["completion"] = task["completion"]
+    entry = event[data_type]
+    uuid = entry["id"]
+    skip = False
+    for entry_s3 in arr_s3:
+        if entry_s3["id"] == uuid:
+            if data_type == "task" and entry_s3["completion"] != 0:
+                # do not allow modifying a deleted task
+                entry_s3["completion"] = entry["completion"]
+            elif data_type == "habit":
+                for dates in entry_s3["completed"]:
+                    if dates == entry["date"]:
+                        skip = True
+                if not skip:
+                    entry_s3["completed"].append(entry["date"])
 
     obj.put(
         Body = (bytes(json.dumps(data, indent = 2).encode('UTF-8'))),
