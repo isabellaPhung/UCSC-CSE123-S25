db04e29281357f4ac939b2fb08fa0627d302668f
Lennan Tuffy <ltuffy@ucsc.edu>
2025-05-20 15:09:46 -0700

    added event deletion, returned data type arrays

--- aws_snippets/lambda_update.py
+++ aws_snippets/lambda_update.py
@@ -11,20 +11,22 @@ def lambda_handler(event, context):
     data = json.loads(obj.get()["Body"].read().decode("utf-8"))
     arr_s3 = data[data_type]
 
-    entry = event[data_type]
-    uuid = entry["id"]
-    skip = False
-    for entry_s3 in arr_s3:
-        if entry_s3["id"] == uuid:
-            if data_type == "task" and entry_s3["completion"] != 0:
-                # do not allow modifying a deleted task
-                entry_s3["completion"] = entry["completion"]
-            elif data_type == "habit":
-                for dates in entry_s3["completed"]:
-                    if dates == entry["date"]:
-                        skip = True
-                if not skip:
-                    entry_s3["completed"].append(entry["date"])
+    for entry in event[data_type]:
+        skip = False
+        uuid = entry["id"]
+        for entry_s3 in arr_s3:
+            if entry_s3["id"] == uuid:
+                if data_type == "task" and entry_s3["completion"] != 0:
+                    # do not allow modifying a deleted task
+                    entry_s3["completion"] = entry["completion"]
+                elif data_type == "habit":
+                    for dates in entry_s3["completed"]:
+                        if dates == entry["date"]:
+                            skip = True
+                    if not skip:
+                        entry_s3["completed"].append(entry["date"])
+                elif data_type == "event":
+                    entry_s3["deleted"] = 1
 
     obj.put(
         Body = (bytes(json.dumps(data, indent = 2).encode('UTF-8'))),
