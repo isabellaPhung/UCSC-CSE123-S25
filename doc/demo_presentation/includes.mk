effd05f7c75ebf39a5f4ed4b3b64e02aff849f5c
marobeck <marobeck@ucsc.edu>
2025-05-22 14:17:46 -0700

    Demo presentation template

---
+++ doc/demo_presentation/includes.mk
@@ -0,0 +1,10 @@
+INSERT_MD := ../common/need_statement.md
+INSERT_MD += ../common/goal_statement.md
+
+INSERT_MD += ../common/design_objective.md
+
+INSERT_MD += ../common/brainstorm.md
+
+INSERT_MD += ../common/decision_table.md
+
+INSERT_MD += ../common/test_plan.md
