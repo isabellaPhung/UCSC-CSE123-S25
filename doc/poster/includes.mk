80ecf2771d7869ad7f53d7fd67fba40ecbf59dd9
Lennan Tuffy <ltuffy@ucsc.edu>
2025-05-26 19:08:24 -0700

    remove aws chart build from ci

--- doc/poster/includes.mk
+++ doc/poster/includes.mk
@@ -1,5 +1,2 @@
 INSERT_MD := ../common/need_statement.md
 INSERT_MD += ../common/goal_statement.md
-
-INSERT_DIAGRAM := ../common/aws_chart/data_flow.py
-INSERT_DIAGRAM += ../common/aws_chart/web_server.py
