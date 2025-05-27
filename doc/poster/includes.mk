098c15f741466e8162fd2f35d31a6ce09a11d29b
Lennan Tuffy <ltuffy@ucsc.edu>
2025-05-26 18:22:27 -0700

    added diagrams to poster build

--- doc/poster/includes.mk
+++ doc/poster/includes.mk
@@ -1,2 +1,5 @@
 INSERT_MD := ../common/need_statement.md
 INSERT_MD += ../common/goal_statement.md
+
+INSERT_DIAGRAM := ../common/aws_chart/data_flow.py
+INSERT_DIAGRAM += ../common/aws_chart/web_server.py
